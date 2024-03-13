7820ebc6806d233f336ec7bfd717fcf70db2ae53
Jeff Adams <jeff.c.adams@outlook.com>
2024-03-12 20:19:15 -0400

    Not working and I have no idea why

---
+++ Canvas.cs
@@ -0,0 +1,69 @@
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TinyAdventure;
+
+public class Canvas
+{
+    public int VirtualWidth { get; }
+    public int VirtualHeight { get; }
+    public Vector2 VirtualCenter { get; }
+    public Matrix VirtualScaleMatrix { get; }
+    public Rectangle RenderRectangle => renderRect;
+
+    private readonly GraphicsDevice graphics;
+    private readonly RenderTarget2D renderTarget;
+    private Rectangle renderRect;
+    
+    public Canvas(GraphicsDevice graphics, int width, int height)
+    {
+        VirtualWidth = width;
+        VirtualHeight = height;
+        VirtualCenter = new Vector2(width * 0.5f, height * 0.5f);
+        VirtualScaleMatrix = Matrix.CreateScale(
+            graphics.Viewport.Width / width, 
+            graphics.Viewport.Height / height, 
+            1.0f);
+
+        this.graphics = graphics;
+
+        renderTarget = new RenderTarget2D(this.graphics, width, height);
+    }
+
+
+    internal void Activate()
+    {          
+        graphics.SetRenderTarget(renderTarget);
+    }
+
+    internal void Draw(SpriteBatch spriteBatch)
+    {
+        graphics.SetRenderTarget(null);
+        graphics.Clear(Color.Black);
+        spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointClamp);
+        spriteBatch.Draw(renderTarget, renderRect, Color.White);
+        spriteBatch.End();
+    }
+
+    internal Rectangle UpdateRenderRectangle()
+    {
+        Rectangle screenSize = graphics.Viewport.Bounds;
+        
+        float scaleX = (float)screenSize.Width / renderTarget.Width;
+        float scaleY = (float)screenSize.Height / renderTarget.Height;
+        float scale = Math.Min(scaleX, scaleY);
+
+        int width = (int)(renderTarget.Width * scale);
+        int height = (int)(renderTarget.Height * scale);
+
+        int x = (screenSize.Width - width) / 2;
+        int y = (screenSize.Height - height) / 2;
+
+        renderRect = new Rectangle(x, y, width, height);
+        return renderRect;
+    }
+
+    public override string ToString() =>
+        $"RenderRect: {renderRect}, RenderTarget: {renderTarget.Bounds}";
+}
---
+++ TheGame.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace TinyAdventure;
+
+public class TheGame : Game
+{
+    private GraphicsDeviceManager graphics;
+    private SpriteBatch spriteBatch;
+    private Canvas canvas;
+
+    private const int gameWidth = 500;
+    private const int gameHeight = 500;
+
+    public TheGame()
+    {
+        graphics = new GraphicsDeviceManager(this)
+        {
+            PreferredBackBufferWidth = GraphicsDevice.DisplayMode.Width,
+            PreferredBackBufferHeight = GraphicsDevice.DisplayMode.Height
+        };
+        graphics.ApplyChanges();
+
+        canvas = new Canvas(GraphicsDevice, gameWidth, gameHeight);
+
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+
+        Window.AllowUserResizing = false;
+        Window.IsBorderless = true;
+        Window.Title = "Tiny Adventure";
+        Window.ClientSizeChanged += UpdateCanvasRenderSize;
+    }
+
+    protected override void Initialize()
+    {
+
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        // TODO: use this.Content to load your game content here
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+
+        // TODO: Add your update logic here
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        canvas.Activate();
+
+        //Draw all the game stuff
+        GraphicsDevice.Clear(Color.SeaGreen);
+
+        canvas.Draw(spriteBatch);
+        base.Draw(gameTime);
+    }
+
+    private void UpdateCanvasRenderSize(object sender, EventArgs e)
+    {
+        canvas.UpdateRenderRectangle();
+    }
+}
